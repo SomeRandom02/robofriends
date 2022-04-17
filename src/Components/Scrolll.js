905d710d87432ac6fa9091817b524afebe5f9761
SomeRandom02 <hoopercolin342@gmail.com>
2022-04-17 03:53:28 -0400

    tweaked height of scroll box

--- src/Components/Scrolll.js
+++ src/Components/Scrolll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scrolll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '5px solid black', height: '600px'}}>
+        <div style={{overflowY: 'scroll', border: '5px solid black', height: '750px'}}>
             {props.children}
         </div>
     )
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import CardList from '../Components/CardList'
 import SearchBox from '../Components/SearchBox'
+import ErrorBoundry from "../Components/ErrorBoundry";
 import './App.css'
 import Scrolll from '../Components/Scrolll'
 // import { findByTestId } from "@testing-library/react";
@@ -36,7 +37,9 @@ class App extends Component {
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox onSearchChange={this.onSearchChange}/>
                     <Scrolll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scrolll>
                 </div>
             );
